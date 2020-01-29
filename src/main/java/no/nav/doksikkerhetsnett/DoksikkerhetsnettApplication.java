381d8f596a1c99b5abd3f02604cda587a5e33806
Marton Skjæveland <Marton.Skjeveland@nav.no>
2020-01-29 13:23:07 +0100

    Init

---
+++ src/main/java/no/nav/doksikkerhetsnett/DoksikkerhetsnettApplication.java
@@ -0,0 +1,13 @@
+package no.nav.doksikkerhetsnett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoksikkerhetsnettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoksikkerhetsnettApplication.class, args);
+	}
+
+}
