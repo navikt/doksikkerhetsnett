6673770b954f80eb698fc2aba00098b59c706396
Marton Skjæveland <Marton.Skjeveland@nav.no>
2020-07-08 15:10:44 +0200

    OH-698: Endret schedule for testing av oppgave-cache i prod

--- core/src/main/java/no/nav/doksikkerhetsnett/DoksikkerhetsnettScheduled.java
+++ core/src/main/java/no/nav/doksikkerhetsnett/DoksikkerhetsnettScheduled.java
@@ -44,7 +44,8 @@ public class DoksikkerhetsnettScheduled {
     }
 
     // Satt til å kjøre klokken 07:00 på man, ons og fre
-    @Scheduled(cron = "0 0 7 * * MON,WED,FRI")
+    //@Scheduled(cron = "0 0 7 * * MON,WED,FRI")
+    @Scheduled(initialDelay = 60_000)
     public void triggerOppdatering() {
         //Kjører read-only temaene
         runDoksikkerhetsnettInReadOnlyMode();
