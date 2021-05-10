d7b20a9dc8919d9dbf9f9fdc0dca614dc44fd072
Joakim Borgersen <joakim.borgersen@nav.no>
2021-05-10 14:36:35 +0200

    MMA-5380: Starte doksnett lesemodus 30 min senere

--- core/src/main/java/no/nav/doksikkerhetsnett/DoksikkerhetsnettScheduled.java
+++ core/src/main/java/no/nav/doksikkerhetsnett/DoksikkerhetsnettScheduled.java
@@ -92,7 +92,7 @@ public class DoksikkerhetsnettScheduled {
     *  Satt til å kjøre klokken 6, mon-fri
     *  Lager grafana metrics på journalposter som er ubehandlede, uten oppgave og har ligget i minst x dager
     */
-	@Scheduled(cron = "0 0 6 * * MON-FRI")
+	@Scheduled(cron = "0 30 6 * * MON-FRI")
 	public void updateDailyGrafanaMetrics() {
 		metricsScheduler.clearOneDayOldCache();
 		metricsScheduler.clearTwoDaysOldCache();
