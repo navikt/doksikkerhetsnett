3fcb38fc2860a9ae4b0541c6620571b575ea55e7
Anders Opskar Voldsund <anders.voldsund@nav.no>
2024-11-01 08:20:21 +0100

    MMA-7657 Fjern ubrukt exception

--- core/src/main/java/no/nav/doksikkerhetsnett/exceptions/functional/FinnOppgaveTillaterIkkeTilknyttingFunctionalException.java
+++
@@ -1,7 +0,0 @@
-package no.nav.doksikkerhetsnett.exceptions.functional;
-
-public class FinnOppgaveTillaterIkkeTilknyttingFunctionalException extends AbstractDoksikkerhetsnettFunctionalException {
-	public FinnOppgaveTillaterIkkeTilknyttingFunctionalException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
